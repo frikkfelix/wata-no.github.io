8726fa673af2aa1b6796e40d949454004fe806e4
Frikk Felix Egge Fjuk <frikk.felix@gmail.com>
2022-06-29 09:03:55 +0200

    add .nojekyll file

--- sections/Solutions.tsx
+++ sections/Solutions.tsx
@@ -7,17 +7,17 @@ export default function Solutions() {
   return (
     <Box id="solutions" sx={{ backgroundColor: '#FFFFFF' }}>
       <Gradient title='Våre løsninger' height='250px'/>
-      <Container sx={{ py: 10 }}>
+      <Container sx={{ py: 4 }}>
         {solutions.map((item, index) => {
           const isEven = index % 2 === 0;
           return (
             <Grid
               container
-              spacing={{ xs: 0, md: 10 }}
+              spacing={{ xs: 0, md: 8 }}
               alignItems='center'
               key={index}
               sx={{
-                my: 10
+                my: {xs: 4, md: 8 }
               }}
             >
               <Grid item xs={12} md={5} order={isEven ? index + 1 : index}>
