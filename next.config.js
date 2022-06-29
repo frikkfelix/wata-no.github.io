a0a0f5a090bb811c03798f07b4d9b3fd3fbcd1df
Frikk Felix Egge Fjuk <frikk.felix@gmail.com>
2022-06-29 09:05:16 +0200

    remove unnecessary code from next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
 }
 
 module.exports = nextConfig
